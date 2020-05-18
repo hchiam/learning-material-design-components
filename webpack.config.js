b3fea24c92bc79917c777b67e070939396d53ba3
hchiam <hchiam@gmail.com>
2020-05-18 12:46:41 -0400

    remove thing that doesn't seem to work

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,5 @@
 const autoprefixer = require("autoprefixer");
 
-const path = require("path");
-
-function tryResolve_(url, sourceFilename) {
-  // Put require.resolve in a try/catch to avoid node-sass failing with cryptic libsass errors
-  // when the importer throws
-  try {
-    return require.resolve(url, { paths: [path.dirname(sourceFilename)] });
-  } catch (e) {
-    return "";
-  }
-}
-
-function tryResolveScss(url, sourceFilename) {
-  // Support omission of .scss and leading _
-  const normalizedUrl = url.endsWith(".scss") ? url : `${url}.scss`;
-  return (
-    tryResolve_(normalizedUrl, sourceFilename) ||
-    tryResolve_(
-      path.join(
-        path.dirname(normalizedUrl),
-        `_${path.basename(normalizedUrl)}`
-      ),
-      sourceFilename
-    )
-  );
-}
-
-function materialImporter(url, prev) {
-  if (url.startsWith("@material")) {
-    const resolved = tryResolveScss(url, prev);
-    return { file: resolved || url };
-  }
-  return { file: url };
-}
-
 module.exports = [
   {
     devServer: {
@@ -66,8 +31,6 @@ module.exports = [
             {
               loader: "sass-loader",
               options: {
-                // importer: materialImporter, // doesn't seem to work
-
                 sassOptions: {
                   includePaths: ["./node_modules"],
                 },
